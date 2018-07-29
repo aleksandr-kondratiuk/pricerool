0eb4c9b524e753521471f5d2af3304cf8ce160c6
Alex Didkovskiy <oididkovskiy@gmail.com>
2018-07-30 02:08:09 +0300

    Builder

--- Builder/Builder.php
+++ Builder/Builder.php
@@ -2,15 +2,13 @@
 
 namespace SP\PriceRool\Builder;
 
-use Magento\Framework\View\Element\Template\Context;
 use Magento\SalesRule\Api\RuleRepositoryInterface;
 use Magento\SalesRule\Api\Data\ConditionInterface;
-use Magento\Framework\View\Element\Template;
 use Magento\SalesRule\Model\Rule;
 use Magento\SalesRule\Model\Rule\Condition\Product\Found;
 use Magento\SalesRule\Model\Rule\Condition\Product as ConditionProduct;
 
-class Builder extends Template
+class Builder extends \Magento\Rule\Model\AbstractModel
 {
     /**
      * SKU argument
@@ -20,10 +18,6 @@ class Builder extends Template
      * @var \Magento\Framework\App\State
      */
     protected $appState;
-    /**
-     * @var Context
-     */
-    protected $context;
 
     /**
      * @var RuleRepositoryInterface
@@ -51,7 +45,6 @@ class Builder extends Template
 
     public function __construct(
 //        \Magento\Framework\App\State $appState,
-        Context $context,
         RuleRepositoryInterface $ruleRepository,
         ConditionInterface $condition,
         Rule $rule,
@@ -59,7 +52,6 @@ class Builder extends Template
         ConditionProduct $conditionProduct
     ) {
 //        $appState->setAreaCode('admin');
-        parent::__construct($context);
         $this->ruleRepository   = $ruleRepository;
         $this->condition        = $condition;
         $this->rule             = $rule;
@@ -71,6 +63,7 @@ class Builder extends Template
     public function createRule()
     {
         $discount = '10';
+        $this->getListProductIdsInRule();
 
         $this->rule->setName('JoshuaFlood_DiscountGenerator')
             ->setDescription('Do not delete! Current product sku - ' .'24-MB01')
@@ -121,4 +114,91 @@ class Builder extends Template
     }
 
 
+    protected $_productIds;
+    /**
+     * Get array of product ids which are matched by rule
+     *
+     * @return array
+     */
+    public function getListProductIdsInRule()
+    {
+        $productCollection = \Magento\Framework\App\ObjectManager::getInstance()->create(
+            '\Magento\Catalog\Model\ResourceModel\Product\Collection'
+        );
+        $productFactory = \Magento\Framework\App\ObjectManager::getInstance()->create(
+            '\Magento\Catalog\Model\ProductFactory'
+        );
+        $this->_productIds = [];
+        $this->setCollectedAttributes([]);
+        $this->getConditions()->collectValidatedAttributes($productCollection);
+        \Magento\Framework\App\ObjectManager::getInstance()->create(
+            '\Magento\Framework\Model\ResourceModel\Iterator'
+        )->walk(
+            $this->_productCollection->getSelect(),
+            [[$this, 'callbackValidateProduct']],
+            [
+                'attributes' => $this->getCollectedAttributes(),
+                'product' => $productFactory->create()
+            ]
+        );
+        return $this->_productIds;
+    }
+    /**
+     * Callback function for product matching
+     *
+     * @param array $args
+     * @return void
+     */
+    public function callbackValidateProduct($args)
+    {
+        $product = clone $args['product'];
+        $product->setData($args['row']);
+        $websites = $this->_getWebsitesMap();
+        foreach ($websites as $websiteId => $defaultStoreId) {
+            $product->setStoreId($defaultStoreId);
+            if ($this->getConditions()->validate($product)) {
+                $this->_productIds[] = $product->getId();
+            }
+        }
+    }
+    /**
+     * Prepare website map
+     *
+     * @return array
+     */
+    protected function _getWebsitesMap()
+    {
+        $map = [];
+        $websites = \Magento\Framework\App\ObjectManager::getInstance()->create(
+            '\Magento\Store\Model\StoreManagerInterface'
+        )->getWebsites();
+        foreach ($websites as $website) {
+            // Continue if website has no store to be able to create catalog rule for website without store
+            if ($website->getDefaultStore() === null) {
+                continue;
+            }
+            $map[$website->getId()] = $website->getDefaultStore()->getId();
+        }
+        return $map;
+    }
+
+    /**
+     * Getter for rule combine conditions instance
+     *
+     * @return \Magento\Rule\Model\Condition\Combine
+     */
+    public function getConditionsInstance()
+    {
+        // TODO: Implement getConditionsInstance() method.
+    }
+
+    /**
+     * Getter for rule actions collection instance
+     *
+     * @return \Magento\Rule\Model\Action\Collection
+     */
+    public function getActionsInstance()
+    {
+        // TODO: Implement getActionsInstance() method.
+    }
 }
