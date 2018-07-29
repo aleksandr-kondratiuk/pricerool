1f3f285f4c8df4b7660285f4c47cf465d1554aa7
Anna Chumak <aleksnx@gmail.com>
2018-07-29 14:31:37 +0300

    upd

---
+++ Block/Display.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @author Pavel Usachev <webcodekeeper@hotmail.com>
+ * @copyright Copyright (c) 2017, Pavel Usachev
+ */
+
+namespace SP\PriceRool\Block;
+
+use Magento\Framework\View\Element\Template;
+
+class Display extends Template
+{
+
+}
