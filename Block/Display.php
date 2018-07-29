92e70b6eaacdde046fdee2c769b55b76f1043ef8
Anna Chumak <aleksnx@gmail.com>
2018-07-29 15:04:51 +0300

    upd

--- Block/Display.php
+++ Block/Display.php
@@ -7,8 +7,28 @@
 namespace SP\PriceRool\Block;
 
 use Magento\Framework\View\Element\Template;
+use Magento\Customer\Model\Session;
 
 class Display extends Template
 {
+    protected $customerSession;
+
+    public function __construct(
+        Template\Context $context,
+        Session $session,
+        array $data = []
+    ) {
+        $this->customerSession= $session;
+        parent::__construct($context, $data);
+    }
+
+    public function _toHtml()
+    {
+        if ($this->customerSession->isLoggedIn()){
+            return parent::_toHtml();
+        } else {
+            return '';
+        }
+    }
 
 }
